e0db684b60e1854d04d350e670671977b557a3e3
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-08-27 21:51:21 -0300

    maximos y minimos
    ejercicio

---
+++ maximosyMinimos.c
@@ -0,0 +1,30 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+    #define esc 27
+
+    int main(){
+
+        char seguir;
+        int nro, max, min,flag=0;
+        do{
+            system("cls");
+            printf("numero:");
+            scanf("%d", &nro);
+            if (flag==0){
+                min=nro;
+                max=nro;
+                flag=1;
+            }
+            if (nro>max){
+                max=nro;
+            }
+            if (nro<min){
+                min=nro;
+            }
+            printf("presione esc para salir, cualquier otra tecla para continuar");
+            seguir=getch();
+        }while(seguir!=esc);
+
+        printf("\n mayor: %d\n menor: %d",max,min);
+    return 0;
+    }
